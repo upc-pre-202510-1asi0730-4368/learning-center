8fc34f735b39877d3dd26c090e56cdceebc3bc5e
Angel Velasquez <pcisavel@upc.edu.pe>
2025-07-01 16:54:04 -0500

    feat(iam): add authentication interceptor.

--- src/shared/services/http.instance.js
+++ src/shared/services/http.instance.js
@@ -4,6 +4,7 @@
  */
 
 import axios from "axios";
+import {authenticationInterceptor} from "../../iam/services/authentication.interceptor.js";
 
 /**
  * Configured axios instance for making HTTP requests
@@ -18,5 +19,8 @@ const httpInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
 });
+
+// Add a request interceptor
+httpInstance.interceptors.request.use(authenticationInterceptor);
 
 export default httpInstance;
